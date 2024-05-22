f182b6a1dee819e252991ca230f1620782e1da89
Galex <galexu@gmail.com>
2024-05-22 19:08:34 +0200

    leerNumerosUsuarios

--- src/NumerosNaturales.java
+++ src/NumerosNaturales.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class NumerosNaturales {
     private List<Integer> listaNum;
@@ -7,5 +11,22 @@ public class NumerosNaturales {
 
     public NumerosNaturales() {
         listaNum = new ArrayList<>();
+    }
+
+    public void leerNumerosUsuario() {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        System.out.println("Introduce numeros naturales o -1 para terminar");
+        while (true) {
+            numero = scanner.nextInt();
+            if (numero == -1) {
+                break;
+            }
+            if (numero > 0) {
+                listaNum.add(numero);
+            } else {
+                System.out.println("Solo se permiten numeros positivos");
+            }
+        }
     }
 }
