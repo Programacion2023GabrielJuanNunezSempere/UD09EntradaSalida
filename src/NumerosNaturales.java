bd1c4569aa36b8cff1150e976721241c0828ab1b
Galex <galexu@gmail.com>
2024-05-22 19:01:22 +0200

    array y constructor

--- src/NumerosNaturales.java
+++ src/NumerosNaturales.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumerosNaturales {
+    private List<Integer> listaNum;
+    private static final String FICHERO = "numNaturales.txt";
 
+    public NumerosNaturales() {
+        listaNum = new ArrayList<>();
+    }
 }
